cd3f382ec064d13c4e2a31bb041def9ab70461f1
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-26 15:17:15 +0530

    Remove duplicacy of fs in wc_test.js

--- test/wc_test.js
+++ test/wc_test.js
@@ -10,10 +10,10 @@ const createFileSystem = function(files) {
 };
 
 describe('wc', function() {
-  it('should return line,  word and character count with filename for single file', function() {
-    const files = { file: 'some file' };
-    const fs = createFileSystem(files);
+  const files = { file: 'some file', emptyFile: '' };
+  const fs = createFileSystem(files);
 
+  it('should return line,  word and character count with filename for single file', function() {
     const actual = wc('file', fs);
     const expected = '\t0\t2\t9 file';
 
@@ -21,9 +21,6 @@ describe('wc', function() {
   });
 
   it('should return line, word and character count as 0 for single empty file', function() {
-    const files = { emptyFile: '' };
-    const fs = createFileSystem(files);
-
     const actual = wc('emptyFile', fs);
     const expected = '\t0\t0\t0 emptyFile';
 
