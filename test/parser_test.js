d2743d7d1403d67f8f49b6ce751a8b02e12d6e58
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-28 11:00:09 +0530

    Group tests for parse()

--- test/parser_test.js
+++ test/parser_test.js
@@ -2,74 +2,80 @@ const assert = require('assert');
 const { parse } = require('../src/io/parser');
 
 describe('parse', function() {
-  it('should parse arguments with only single file', function() {
-    const args = ['file'];
-    const actual = parse(args);
-    const expected = {
-      filenames: ['file'],
-      options: ['lineCount', 'wordCount', 'charCount']
-    };
+  describe('for single file', function() {
+    it('should parse argments for no options specified', function() {
+      const args = ['file'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file'],
+        options: ['lineCount', 'wordCount', 'charCount']
+      };
 
-    assert.deepEqual(actual, expected);
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should parse arguments with single option', function() {
+      const args = ['-l', 'file'];
+      const actual = parse(args);
+      const expected = { filenames: ['file'], options: ['lineCount'] };
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should parse arguments with two options together', function() {
+      const args = ['-lw', 'file'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file'],
+        options: ['lineCount', 'wordCount']
+      };
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should parse arguments with three options together', function() {
+      const args = ['-lwc', 'file'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file'],
+        options: ['lineCount', 'wordCount', 'charCount']
+      };
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should parse arguments with two option separated with space', function() {
+      const args = ['-l', '-w', 'file'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file'],
+        options: ['lineCount', 'wordCount']
+      };
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should parse arguments with three options separated with space', function() {
+      const args = ['-l', '-w', '-c', 'file'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file'],
+        options: ['lineCount', 'wordCount', 'charCount']
+      };
+
+      assert.deepEqual(actual, expected);
+    });
   });
+  describe('for multiple files', function() {
+    it('should parse arguments with no option specified', function() {
+      const args = ['file', 'file2', 'file3'];
+      const actual = parse(args);
+      const expected = {
+        filenames: ['file', 'file2', 'file3'],
+        options: ['lineCount', 'wordCount', 'charCount']
+      };
 
-  it('should parse arguments with only single file and single option', function() {
-    const args = ['-l', 'file'];
-    const actual = parse(args);
-    const expected = { filenames: ['file'], options: ['lineCount'] };
-
-    assert.deepEqual(actual, expected);
-  });
-
-  it('should parse arguments with only single file and two options together', function() {
-    const args = ['-lw', 'file'];
-    const actual = parse(args);
-    const expected = { filenames: ['file'], options: ['lineCount', 'wordCount'] };
-
-    assert.deepEqual(actual, expected);
-  });
-
-  it('should parse arguments with only single file and three options together', function() {
-    const args = ['-lwc', 'file'];
-    const actual = parse(args);
-    const expected = {
-      filenames: ['file'],
-      options: ['lineCount', 'wordCount', 'charCount']
-    };
-
-    assert.deepEqual(actual, expected);
-  });
-
-  it('should parse arguments with only single file and two option separated with space', function() {
-    const args = ['-l', '-w', 'file'];
-    const actual = parse(args);
-    const expected = {
-      filenames: ['file'],
-      options: ['lineCount', 'wordCount']
-    };
-
-    assert.deepEqual(actual, expected);
-  });
-
-  it('should parse arguments with only single file and three options separated with space', function() {
-    const args = ['-l', '-w', '-c', 'file'];
-    const actual = parse(args);
-    const expected = {
-      filenames: ['file'],
-      options: ['lineCount', 'wordCount', 'charCount']
-    };
-
-    assert.deepEqual(actual, expected);
-  });
-
-  it('should parse arguments with multiple files and no option specified', function() {
-    const args = ['file', 'file2', 'file3'];
-    const actual = parse(args);
-    const expected = {
-      filenames: ['file', 'file2', 'file3'],
-      options: ['lineCount', 'wordCount', 'charCount']
-    };
-
-    assert.deepEqual(actual, expected);
+      assert.deepEqual(actual, expected);
+    });
   });
 });
